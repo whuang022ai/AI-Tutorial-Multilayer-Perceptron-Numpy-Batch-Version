4c08eee8d698933a1fd6ffc1481eb6e236d43c2d
whuang022ai <whuang022.ai@gmail.com>
2019-11-06 01:27:33 +0800

    spilt act fx to act.py from mlp.py

--- mlp.py
+++ mlp.py
@@ -8,116 +8,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-from abc import ABCMeta, abstractmethod
-
-
-class Activate_Function(metaclass=ABCMeta):
-
-    @abstractmethod
-    def f(self, x):
-        pass
-
-    @abstractmethod
-    def df(self, y):
-        pass
-
-    def plot_fx_dfx(self, min, max, inter, title):
-
-        x = np.arange(min, max, inter)
-        f_vect = np.vectorize(self.f)
-        df_vect = np.vectorize(self.df)
-        plt.figure(title)
-        plt.subplot(211)
-        plt.ylabel('f(x)')
-        plt.plot(x, f_vect(x))
-        plt.grid(True)
-        plt.subplot(212)
-        plt.ylabel('d f(x)')
-        plt.plot(x, df_vect(f_vect(x)), color='red',
-                 linewidth=1.0, linestyle='--')
-        plt.grid(True)
-        plt.show()
-
-    @abstractmethod
-    def show_plot(self):
-        pass
-
-
-class Activate_Function_Sigmoid(Activate_Function):
-
-    def f(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def df(self, y):
-        return y*(1-y)
-
-    def show_plot(self):
-        self.plot_fx_dfx(-5.0, 5.0, 0.1, 'The Sigmoid Function')
-
-
-class Activate_Function_Tanh(Activate_Function):
-
-    def f(self, x):
-        return np.tanh(x)
-
-    def df(self, y):
-        return 1-y**2
-
-    def show_plot(self):
-        self.plot_fx_dfx(-5.0, 5.0, 0.1, 'The Tanh Function')
-
-
-class Activate_Function_Relu(Activate_Function):
-
-    def __init__(self):
-        self.lrelu = Activate_Function_LeakyRelu()
-        self.lrelu.alpha = 0.0
-
-    def f(self, x):
-        return self.lrelu.f(x)
-
-    def df(self, y):
-        return self.lrelu.df(y)
-
-    def show_plot(self):
-        self.plot_fx_dfx(-5.0, 5.0, 0.01, 'The ReLU Function')
-
-
-class Activate_Function_LeakyRelu(Activate_Function):
-
-    def __init__(self):
-        self.alpha = 0.1
-
-    def f(self, x):
-        if x > 0:
-            return x
-        else:
-            return (self.alpha*x)
-
-    def df(self, y):
-        if y > 0:
-            return 1.0
-        else:
-            return self.alpha
-
-    def show_plot(self):
-        self.plot_fx_dfx(-5.0, 5.0, 0.01, 'The Leaky ReLU Function')
-
-
-class Activate_Function_Generator():
-
-    def __init__(self, name):
-
-        if name == 'sigmoid':
-            self.get = Activate_Function_Sigmoid()
-        elif name == 'tanh':
-            self.get = Activate_Function_Tanh()
-        elif name == 'relu':
-            self.get = Activate_Function_Relu()
-        elif name == 'leaky-relu':
-            self.get = Activate_Function_LeakyRelu()
-
+from actf import Activate_Function_Generator
 
 class MLP():
 
@@ -216,7 +107,7 @@ class MLP():
 if __name__ == "__main__":
 
     # leaky-relu recommand setting for xor : epoh=200 , lr =0.07 , act.alpha=0.2
-    draw_mse = True # display mse realtime
+    draw_mse = True  # display mse realtime
     mlp = MLP(2, 4, 5, 1)
     X = np.array(
         [
